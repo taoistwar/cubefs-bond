b3c0d63bf7efb667853ece0e112b508dc1b87f52
hongdao_ning <taoistwar@163.com>
2022-09-19 17:12:30 +0800

    rocket

--- src/controller.rs
+++ src/controller.rs
@@ -1,41 +1,103 @@
+use std::fs;
 use std::path::Path;
 use std::thread::sleep;
 use std::time::Duration;
-use std::{collections::HashMap, fs};
 
 use std::process::Command;
 
-use salvo::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[allow(unused, non_snake_case)]
+#[derive(Debug, Serialize, Deserialize, Default)]
+struct ClientConfig {
+    mountPoint: Option<String>, // 挂载点，是
+
+    volName: Option<String>, // 卷名称，是
+
+    owner: Option<String>, // 所有者，是
+
+    masterAddr: String, // Master节点地址，是
+
+    logDir: Option<String>, // 日志存放路径，否
+
+    logLevel: Option<String>, // 日志级别：debug, info, warn, error， 否
+
+    profPort: Option<String>, // golang pprof调试端口，否
+
+    exporterPort: Option<String>, // prometheus获取监控数据端口，否
+
+    consulAddr: Option<String>, // 监控注册服务器地址，否
+
+    lookupValid: Option<String>, // 内核FUSE lookup有效期，单位：秒，否
+
+    attrValid: Option<String>, // 内核FUSE attribute有效期，单位：秒，否
+
+    icacheTimeout: Option<String>, //客户端inode cache有效期，单位：秒；否
+
+    enSyncWrite: Option<String>, // 使能DirectIO同步写，即DirectIO强制数据节点落盘；否
+
+    autoInvalData: Option<String>, // FUSE挂载使用AutoInvalData选项；否
+
+    ronly: Option<bool>, // 以只读方式挂载，默认为false，否
+
+    writecache: Option<bool>, // 利用内核FUSE的写缓存功能，需要内核FUSE模块支持写缓存，默认为false；否
+
+    keepcache: Option<bool>, // 保留内核页面缓存。此功能需要启用writecache选项，默认为false；否
+
+    token: Option<String>, // 如果创建卷时开启了enableToken，此参数填写对应权限的token；否
+
+    readRate: Option<u32>, // 限制每秒读取次数，默认无限制；否
+
+    writeRate: Option<u32>, // 限制每秒写入次数，默认无限制；否
+
+    followerRead: Option<bool>, // 从follower中读取数据，默认为false；否
+
+    accessKey: Option<String>, //卷所属用户的鉴权密钥；否
+
+    secretKey: Option<String>, // 卷所属用户的鉴权密钥；否
+
+    disableDcache: Option<bool>, // 禁用Dentry缓存，默认为false；否
+
+    subdir: Option<String>, // 设置子目录挂载；否
+
+    fsyncOnClose: Option<bool>, // 文件关闭后执行fsync操作，默认为true；否
+
+    maxcpus: Option<u32>, // 最大可使用的cpu核数，可限制client进程cpu使用率；否
+
+    enableXattr: Option<bool>, // 是否使用*xattr*，默认是false；否
+
+    enableBcache: Option<bool>, // 是否开启本地一级缓存，默认false
+}
 
 struct Bond {
-    config: HashMap<String, String>,
+    config: ClientConfig,
     log_path: String,
     config_file: String,
 }
 
 impl Bond {
-    fn setup(config: HashMap<String, String>) -> Result<Bond, String> {
+    fn setup(config: ClientConfig) -> Result<Bond, String> {
         let mut config = config;
-        let volume_name = match config.get("volName") {
-            Some(v) => v.clone(),
-            None => return Err("volName missing".to_string()),
-        };
-        match config.get("masterAddr") {
-            Some(v) => v.clone(),
-            None => return Err("masterAddr missing".to_string()),
-        };
+
+        if config.volName.is_none() {
+            return Err("volName missing".to_string());
+        }
+        if config.masterAddr.is_empty() {
+            return Err("masterAddr missing".to_string());
+        }
+        let volume_name = &config.volName.as_ref().unwrap();
 
         let mount_file_path = format!("/cfs/mount/{}", volume_name);
-        config.insert("mountPoint".to_string(), mount_file_path);
+        config.mountPoint = Some(mount_file_path);
         let log_path = format!("/cfs/client/{}", &volume_name);
-        config.insert("logDir".to_string(), log_path.clone());
+        config.logDir = Some(log_path.clone());
 
         let config_file = format!("/cfs/client/{}/config.json", volume_name);
-        if config.get("logLevel").is_none() {
-            config.insert("logLevel".to_string(), "info".to_string());
+        if config.logLevel.is_none() {
+            config.logLevel = Some("info".to_string());
         }
-        if config.get("owner").is_none() {
-            config.insert("owner".to_string(), "cfs".to_string());
+        if config.owner.is_none() {
+            config.owner = Some("cfs".to_string());
         }
         let bond = Bond {
             config,
@@ -115,72 +177,76 @@ impl Bond {
     }
 }
 
-#[handler]
-pub async fn mount(req: &mut Request, res: &mut Response) {
-    let config = req.parse_body::<HashMap<String, String>>().await;
+#[post("/mount", data = "<input>")]
+pub fn mount(input: Option<String>) -> String {
+    if input.is_none() {
+        return "body is empty".to_string();
+    }
+
+    let config = serde_json::from_str(input.unwrap().as_str());
     if let Err(e) = config {
-        res.render(Text::Plain(format!("parse body fail: {}", e)));
-        return;
+        return format!("parse body fail: {}", e);
     }
     let config = config.unwrap();
     // 1. setup and start
     let bond = match Bond::setup(config) {
         Ok(v) => v,
         Err(e) => {
-            res.render(Text::Plain(format!("fail: parse param, {}", e)));
-            return;
+            return format!("fail: parse param, {}", e);
         }
     };
 
     // 2. start cfs-client
     match bond.startup() {
-        Ok(v) => {
-            res.render(Text::Plain(v));
-        }
+        Ok(v) => v,
         Err(e) => {
-            res.render(Text::Plain(format!("fail: start client fail, {}", e)));
+            format!("fail: start client fail, {}", e)
         }
     }
 }
 
-#[handler]
-pub async fn umount(req: &mut Request, res: &mut Response) {
-    let volume_name = req.param::<String>("volume_name");
+#[get("/umount/<volume_name>")]
+pub fn umount(volume_name: Option<String>) -> String {
     if volume_name.is_none() {
-        res.render(Text::Plain("fail: volume_name missing"));
-        return;
+        return "fail: volume_name missing".to_string();
     }
     let volume_name = volume_name.unwrap();
     if volume_name.is_empty() {
-        res.render(Text::Plain("fail: volume_name empty"));
-        return;
+        return "fail: volume_name empty".to_string();
     }
 
     let shell = format!("umount /cfs/mount/{}", volume_name);
     match Command::new("/bin/sh").arg("-c").arg(&shell).output() {
         Ok(output) => match String::from_utf8(output.stdout) {
             Ok(v) => {
-                res.render(Text::Plain(v));
+                return v;
             }
             Err(e) => {
-                res.render(Text::Plain(format!("fail: parse shell output fail, {}", e)));
+                return format!("fail: parse shell output fail, {}", e).to_string();
             }
         },
         Err(e) => {
-            res.render(Text::Plain(format!(
-                "fail: start[{}] fail, output:{}\n",
-                &shell, e
-            )));
+            return format!("fail: start[{}] fail, output:{}\n", &shell, e);
         }
     }
 }
 
 #[cfg(test)]
 mod test {
-    use std::collections::HashMap;
+    use super::{Bond, ClientConfig};
 
-    use super::Bond;
-
+    #[test]
+    fn test_serde() {
+        let input = r#"{
+            "volName": "test",
+            "owner": "cfs",
+            "masterAddr": "10.201.3.28:8868,10.201.3.29:8868,10.201.3.30:8868",
+            "profPort": "17510",
+            "exporterPort": "9504"
+          }"#;
+        let x: ClientConfig = serde_json::from_str(input).unwrap();
+        println!("{:?}", x)
+    }
     #[test]
     fn test() {
         let input = r#"{
@@ -191,7 +257,7 @@ mod test {
             "exporterPort": "9504"
           }"#;
         // 1. setup and start
-        let config: HashMap<String, String> = serde_json::from_str(input).unwrap();
+        let config: ClientConfig = serde_json::from_str(input).unwrap();
         let bond = match Bond::setup(config) {
             Ok(v) => v,
             Err(e) => {
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,18 @@
 mod controller;
 
-use salvo::prelude::*;
+#[macro_use]
+extern crate rocket;
 
-#[handler]
+#[get("/")]
 fn index() -> &'static str {
     r#"
-        API  URI: /bond
+        API  URI: /mount
              BODY: {}
 
     body is json, and for cubefs client config.json content
 "#
 }
-#[cfg(target_os = "linux")]
-#[tokio::main]
-async fn main() {
-    use std::{fs, path::Path};
-    let path = "/cfs/bond/salvo.sock";
-    let sock_path = Path::new(path);
-    if sock_path.exists() {
-        fs::remove_file(sock_path).expect("unix socket file exist and can't remove");
-    }
-
-    let router = Router::new()
-        .get(index)
-        .push(Router::with_path("/mount/<volume_name>").delete(controller::mount))
-        .push(Router::with_path("/bond/<volume_name>").delete(controller::mount))
-        .push(Router::with_path("/umount/<volume_name>").delete(controller::umount));
-
-    Server::new(UnixListener::bind(path)).serve(router).await;
-}
-
-#[cfg(not(target_os = "linux"))]
-#[tokio::main]
-async fn main() {
-    println!("please run cubefs-bond in linux");
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, controller::mount, controller::umount])
 }
